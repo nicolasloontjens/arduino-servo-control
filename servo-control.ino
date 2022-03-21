0151eae2195f5cf7002861352872907118ae2ddf
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-03-21 16:56:28 +0100

    motor control

--- servo-control.ino
+++ servo-control.ino
@@ -1,9 +1,15 @@
 #include <Servo.h>
 
 Servo myservo; //servo object initialization
+int* parsedInts = {};
+
+int angle = 0;
+int inputspeed = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(DAC0,OUTPUT);
+  pinMode(50,OUTPUT);
   Serial.begin(9600);
   while(!Serial){
     ;
@@ -16,25 +22,40 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
     String input = Serial.readString();
-    int* parsedInts = parseInput(input);
-    moveServo(parsedInts[0],parsedInts[1]);
-    
+    parseInput(input);
+    Serial.println(angle);
+    Serial.println(inputspeed);
+    moveServo(angle);
+    adjustMotorSpeed(inputspeed); 
   }
 }
 
-int* parseInput(String input){
+void parseInput(String input){
   int index = input.indexOf(';');
-  String angle = input.substring(0,index);
-  String inputspeed = input.substring(index+1,input.length());
-  Serial.println(angle);
-  Serial.println(inputspeed);
-  int result[] = {angle.toInt(), inputspeed.toInt()};
-  return result;
-  //return "Angle: " + angle + " Speed: " + inputspeed;
+  String a = input.substring(0,index);
+  String i = input.substring(index+1,input.length());
+  angle = a.toInt();
+  inputspeed = i.toInt();
 }
 
-void moveServo(int pos, int thespeed){
+void moveServo(int pos){
   String output = "Moving to angle: ";
   Serial.println(output + pos);
   myservo.write(pos);
+}
+
+void adjustMotorSpeed(int motorspeed){
+  //double analogspeed = motorspeed * 2.55;
+  if(motorspeed <= 0){
+    digitalWrite(50,LOW);
+  }else{
+    digitalWrite(50,HIGH);
+  }
+  delay(500);
+  if(motorspeed > 255){
+    motorspeed = 255;
+  }
+  Serial.print(" Motorspeed :");
+  Serial.println(motorspeed);
+  analogWrite(DAC0,abs(motorspeed));
 }
